294d593ee3c4c5346b9b93dd2592bbc74640370f
AMD <AMD@DESKTOP-2K7AFGK>
2023-05-24 16:30:06 -0500

    Se agrego el correo al acceso del usuario y algunos arreglos de orotgrafía

--- src/main/java/Conexionbd/user.java
+++ src/main/java/Conexionbd/user.java
@@ -1,4 +1,5 @@
 package Conexionbd;
+
 import Conexionbd.Conexion;
 import Interfaz.Logueo;
 
@@ -11,32 +12,71 @@ import javax.swing.JTextField;
 import java.sql.Connection;
 
 public class user {
-	
+
+	private String codigo;
+	private String correo;
+	private String codempleado;
+
 	public void validar(JTextField cod) {
 		try {
-			
-		
-		ResultSet rs = null;
-		PreparedStatement ps = null;
-		Conexionbd.Conexion c = new Conexionbd.Conexion();
-		String sql= "SELECT * FROM empleado where codigo = (?);";
-		ps= c.establecerConexion().prepareStatement(sql);
-		ps.setString(1, cod.getText().toUpperCase());
-		rs=ps.executeQuery();
-		
-		if (rs.next()) {
-			JOptionPane.showMessageDialog(null, "ingreso");
-			 Logueo l = new Logueo();
-			 l.setVisible(true);
-		} else {
 
-			JOptionPane.showMessageDialog(null, "INCORRECTO");
-		}
+			ResultSet rs = null;
+			PreparedStatement ps = null;
+			Conexionbd.Conexion c = new Conexionbd.Conexion();
+			String sql = "SELECT * FROM empleado where codigo = (?);";
+			ps = c.establecerConexion().prepareStatement(sql);
+			ps.setString(1, cod.getText().toUpperCase());
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+				JOptionPane.showMessageDialog(null, "Ingreso");
+				Logueo l = new Logueo();
+				l.setVisible(true);
+			} else {
+
+				JOptionPane.showMessageDialog(null, "INCORRECTO");
+			}
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "error"+e.toString());
+			JOptionPane.showMessageDialog(null, "error" + e.toString());
 		}
-		
 
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getCodempleado() {
+		return codempleado;
+	}
+
+	public void setCodempleado(JTextField codigo) {
+		try {
+			ResultSet rs = null;
+			PreparedStatement ps = null;
+			Conexionbd.Conexion c = new Conexionbd.Conexion();
+			String sql = "select codempleado from empleado where codigo = (?);";
+			ps = c.establecerConexion().prepareStatement(sql);
+			ps.setString(1, codigo.getText().toUpperCase());
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				codempleado = rs.getString("codempleado");
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "error" + e.toString());
+		}
 	}
 
 }
